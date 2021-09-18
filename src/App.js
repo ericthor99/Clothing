c9b97ddb57bc9334d3ead9dc938d2298e5af1cb3
ericthor99 <ericrt99@hotmail.com>
2021-09-17 21:57:55 -0700

    update with shop page & previews

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './Components/homepage.comp';
+import ShopPage from './Components/shop.comp';
 import './Components/homepage.styles.scss';
 
-const HatsPage = () => (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-)
+ 
 
 function App() {
   return ( <div> 
     <Switch>
       
-      <Route exact path='/shop/hats' component = { HatsPage } />
+      <Route exact path='/shop' component = { ShopPage } />
       <Route   path='/' component = { HomePage } />
     </Switch>
    </div> );
